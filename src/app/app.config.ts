9a6bb8d5f2f8b32d754fde8cce0ba673cd97891a
Gabriel Christino <gabriel.christino@gmail.com>
2025-10-18 20:22:32 -0300

    ajustes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations'; // 1. Importe aqui
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withRouterConfig({ onSameUrlNavigation: 'reload' })
+    ),
     provideAnimations() // 2. Adicione aqui
   ]
 };
