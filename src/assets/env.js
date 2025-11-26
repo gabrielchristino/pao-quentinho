1349d277c0eac73facd3ef8065ebb96b9c365445
Gabriel Christino <gabriel.christino@gmail.com>
2025-11-26 05:59:53 -0800

    fix: imports styles

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,3 +1,3 @@
 window.__env = window.__env || {};
-window.__env.API_URL = 'https://pao-quentinho-backend-develop.up.railway.app/api';
+window.__env.API_URL = 'http://localhost:3000/api';
 window.__env.FRONTEND_URL = 'http://localhost:4200';
