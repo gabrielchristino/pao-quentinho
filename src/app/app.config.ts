618fe5fb133d0dc00b890fb398294b158e36a619
Gabriel Christino <gabriel.christino@gmail.com>
2025-12-05 16:22:48 -0300

    fix: notification service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withRouterConfig } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations'; // 1. Importe aqui
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './services/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +14,11 @@ export const appConfig: ApplicationConfig = {
       withRouterConfig({ onSameUrlNavigation: 'reload' })
     ),
     provideAnimations(),
-    provideHttpClient(withInterceptors([authInterceptor]))
-
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+        // Habilita o Service Worker. Em produção, é comum usar !isDevMode()
+        enabled: true,
+        registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
